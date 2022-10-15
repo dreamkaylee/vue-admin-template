239c79f146d6c27443a55e8fe3f751c7f39592be
雷锋2班的红领巾 <dreamkaylee@foxmail.com>
2022-10-15 22:15:43 +0800

    refactor: 调整git的文件忽略

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,18 +1,6 @@
 module.exports = {
   root: true,
   extends: ['stylelint-config-standard-vue', 'stylelint-config-prettier'],
-  overrides: [
-    {
-      files: ['*.scss', '**/*.scss'],
-      customSyntax: 'postcss-scss',
-      extends: ['stylelint-config-standard-scss']
-    },
-    {
-      files: ['*.less', '**/*.less'],
-      customSyntax: 'postcss-less',
-      extends: ['stylelint-config-standard-less']
-    }
-  ],
   rules: {
     'selector-pseudo-element-no-unknown': [
       true,
