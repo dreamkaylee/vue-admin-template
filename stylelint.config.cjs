d9630274f2204af92c34447cd002c840806f81b5
雷锋2班的红领巾 <dreamkaylee@foxmail.com>
2022-10-22 14:46:34 +0800

    build: 升级依赖

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,6 +2,13 @@ module.exports = {
   root: true,
   extends: ['stylelint-config-standard-vue', 'stylelint-config-prettier'],
   rules: {
+    'no-empty-source': null,
+    'selector-class-pattern': [
+      '^[5A-Za-z0-9-_]+$',
+      {
+        message: 'Expected class selector to be kebab-case'
+      }
+    ],
     'selector-pseudo-element-no-unknown': [
       true,
       {
